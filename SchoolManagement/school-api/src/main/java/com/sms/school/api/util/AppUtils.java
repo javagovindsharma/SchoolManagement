5a7caa1b7c07067091996e7aebc6c1a91bdb8cce
javagovindsharma <javagovindsharma@gmail.com>
2026-07-02 14:31:23 +0530

    video chnages

--- SchoolManagement/school-api/src/main/java/com/sms/school/api/util/AppUtils.java
+++
@@ -1,5 +0,0 @@
-
-package com.sms.school.api.util;
-
-public class AppUtils {
-}
--- SchoolManagement/school-common/src/main/java/com/sms/school/common/constant/AppConstants.java
+++
@@ -1,6 +0,0 @@
-
-package com.sms.school.common.constant;
-
-public class AppConstants {
-    public static final String SUCCESS = "SUCCESS";
-}
